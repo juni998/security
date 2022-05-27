078fbbc3d794541d1d86d5f3ac6cf1fb7c5c6d2c
YJBAEK <wodnrdlgla@naver.com>
2022-05-27 22:08:34 +0900

    #4 SecurityConfig 권장하는 클래스로 변경

--- src/main/java/com/security/security/SecurityConfig.java
+++ src/main/java/com/security/security/SecurityConfig.java
@@ -1,13 +1,16 @@
 package com.security.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -17,17 +20,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers("/resources/**");
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         /* 인가 */
-        http
+        return http
                 .authorizeRequests()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
         /* 인증 */
-        http
+        .and()
                 .formLogin() //form 로그인 인증 기능 작동
                 //.loginPage("/loginPage") // login 페이지 설정
                 .defaultSuccessUrl("/") // 인증 성공시 이동할 페이지
@@ -49,7 +55,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         response.sendRedirect("/loginPage");
                     }
                 })
-                .permitAll(); // 접근 가능 설정
+                .permitAll()  // 접근 가능 설정
+                .and().build();
 
     }
 }
