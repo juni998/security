6a9e758f7d88728ca47e7617523e6f68f24b96ff
YJBAEK <wodnrdlgla@naver.com>
2022-05-27 21:53:37 +0900

    #1 security start

---
+++ src/main/java/com/security/security/SecurityController.java
@@ -0,0 +1,20 @@
+package com.security.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
+
+}
